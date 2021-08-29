2828e09f0ab86348d7936d8327ab4bf4753c63d4
mwickramanayaka <manulwick@gmail.com>
2021-08-29 19:31:15 +0530

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log("Hello, ninjas", e);
+  };
+
+  const handleClickAgain = (name, e) => {
+    console.log("hello " + name, e.target);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={(e) => handleClickAgain("manul", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
