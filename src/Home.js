2c1fac062e87c72a323ca2c0f52d1ca20fb94949
mwickramanayaka <manulwick@gmail.com>
2021-09-01 22:49:32 +0530

    delete blog

--- src/Home.js
+++ src/Home.js
@@ -19,68 +19,49 @@ const Home = () => {
       title: "Let’s start Performance testing with JMeter",
       body: "lorem ipsum...",
       author: "Manul Wickramanayaka",
-      id: 2,
-    },
-    {
-      title: "Getting started with TestNG",
-      body: "lorem ipsum...",
-      author: "Manul Wickramanayaka",
-      id: 2,
+      id: 3,
     },
     {
       title:
         "How to do Multiple Browser Parallel Testing in a selenium TestNG project",
       body: "lorem ipsum...",
       author: "Manul Wickramanayaka",
-      id: 2,
+      id: 4,
     },
     {
       title: "Waits in Selenium",
       body: "lorem ipsum...",
       author: "Manul Wickramanayaka",
-      id: 3,
-    },
-    {
-      title: "Using WebDriver Manager with Maven POM dependency",
-      body: "lorem ipsum...",
-      author: "Manul Wickramanayaka",
-      id: 3,
-    },
-    {
-      title: "How to use Extent Reports with TestNG in Selenium Java",
-      body: "lorem ipsum...",
-      author: "Manul Wickramanayaka",
-      id: 3,
+      id: 5,
     },
     {
       title: "Dealing with Jar files from command line in a Selenium Project",
       body: "lorem ipsum...",
       author: "Manul Wickramanayaka",
-      id: 3,
+      id: 6,
     },
     {
       title: "Working with TestNG Listeners in Selenium Webdriver",
       body: "lorem ipsum...",
       author: "Rick Grimes",
-      id: 3,
+      id: 7,
     },
     {
       title: "How to Read/Write Data from Excel File-Apache POI",
       body: "lorem ipsum...",
       author: "Rick Grimes",
-      id: 3,
-    },
-    {
-      title: "Logging with Apache Log4J",
-      body: "lorem ipsum...",
-      author: "Rick Grimes",
-      id: 3,
+      id: 8,
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
       <BlogList
         blogs={blogs.filter((blog) => blog.author == "Rick Grimes")}
         title="Rick's Blogs"
