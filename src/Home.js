6ce54594eb4eecc833e22fca32c4958003152863
mwickramanayaka <manulwick@gmail.com>
2021-09-01 22:59:57 +0530

    delete blog

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -58,6 +58,11 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log("useEffect ran...");
+    console.log(blogs);
+  });
 
   return (
     <div className="home">
